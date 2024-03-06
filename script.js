d9074b2c67d4271a97700328c0e94f50f494d370
AnasCoding1986 <anassust.scw@gmail.com>
2024-03-07 04:27:32 +0600

    second commit

--- script.js
+++ script.js
@@ -3,3 +3,14 @@
 // Single data details: https://openapi.programming-hero.com/api/ai/tool/${id}
 
 // Single data Example: https://openapi.programming-hero.com/api/ai/tool/01
+
+
+const fetchAllData = async () => {
+    const url = `https://openapi.programming-hero.com/api/ai/tools`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data.data.tools);
+}
+
+fetchAllData();
+
